5908e231d654841aa758cd41c94bf90d4d351893
bashpsk <sanchana3344@gmail.com>
2026-05-07 17:12:42 +0530

    Adding WaveAnimation.kt

--- app/src/main/kotlin/io/bashpsk/emptylibs/MainActivity.kt
+++ app/src/main/kotlin/io/bashpsk/emptylibs/MainActivity.kt
@@ -68,6 +68,7 @@ class MainActivity : ComponentActivity() {
 //                    TransformImageScreen()
 //                    ZoomableLayoutScreen()
 //                    VideoGestureScreen()
+//                    WaveAnimationScreen()
 //                    WheelTextPickerScreen()
                 }
             }
